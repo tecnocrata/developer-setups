0556a6ce5b13555b9b1195b09b629d6faf788e8c
Enrique Ortuño <atarconcet@gmail.com>
2021-06-20 12:46:34 -0400

    brew installation updated

--- macos/setup.sh
+++ macos/setup.sh
@@ -5,7 +5,8 @@ cd ~/installation
 # =================
 # INSTALLING BREW
 # =================
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # =================
 # INSTALLING GIT & NODE
